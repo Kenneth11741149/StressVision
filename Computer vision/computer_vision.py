59205e6a87e9e10cae383db88794a37ad372319d
Kenneth11741149 <burningion@hotmail.com>
2021-06-24 11:53:29 -0600

    asdasdasd

--- Computer vision/computer_vision.py
+++ Computer vision/computer_vision.py
@@ -15,9 +15,13 @@ cap.set(3,frameWidth)
 cap.set(4,frameHeight)
 
 
-colors = [
+rubiks_colors = [
             [95,75,51,125,255,255], #Blue
-            [19,187,60,88,255,111]
+            [26,104,33,96,255,69], #Green
+            [0,107,95,4,255,164], #Red
+            [0,156,143,37,255,172], #Orange
+            [62,0,103,119,255,148], #White
+            [10,114,108,82,255,217] #Yellow
 
 ]
 
@@ -59,8 +63,8 @@ def preprocess(image):
     imgCanny = cv2.Canny(imgBlur, 50, 50)
     kernel = np.ones((5, 5))
     imgDial = cv2.dilate(imgCanny, kernel, iterations=2)
-    imgThres = cv2.erode(imgDial, kernel, iterations=3)
-    return imgCanny
+    imgThres = cv2.erode(imgDial, kernel, iterations=1)
+    return imgThres
 
 def getContour(img): #An image of contours aka the canny edge detection image.
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, # Second param is retrieval method. There are several, we will use external method. It retrieves the extreme outer contours.
@@ -69,18 +73,30 @@ def getContour(img): #An image of contours aka the canny edge detection image.
 
     for cnt in contours: #cnt is an array of basically the contour of what was detected.
         area = cv2.contourArea(cnt)
-        if area > 500 and area < 3000:
-
+        if area > 400 and area < 7000:
+            # cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 3)  # -1 = draw all contours.
             cv2.imshow("imgContoursDrawn",imgContour)
             peri = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
             x, y, w, h = cv2.boundingRect(approx)
-            if len(approx) == 4:
-             cv2.drawContours(imgContour, cnt, -1, (255,0,255), 3)  # -1 = draw all contours.
-             # cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # if len(approx) == 4:
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 3)  # -1 = draw all contours.
             cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     return imgContour
+
+def findColor(img):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    for color in rubiks_colors:
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv2.inRange(imgHSV,lower,upper)
+        kernel = np.ones((5, 5))
+        imgDial = cv2.dilate(mask, kernel, iterations=2)
+        imgThres = cv2.erode(mask, kernel, iterations=1)
+        getContour(imgThres)
+    # return newPoints
 
 
 while True:
@@ -88,10 +104,12 @@ while True:
     imgContour = img.copy()
 
     imgCanny = preprocess(img)
-    imgContourDrawn = getContour(imgCanny)
+    # imgContourDrawn = getContour(imgCanny)
+    findColor(img)
 
-    stack_of_images = stackImages(0.6,([img, imgCanny, imgContourDrawn]))
-    cv2.imshow("Project Output", stack_of_images)
+
+    # stack_of_images = stackImages(0.6,([img, imgCanny, imgContourDrawn]))
+    # cv2.imshow("Project Output", stack_of_images)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
